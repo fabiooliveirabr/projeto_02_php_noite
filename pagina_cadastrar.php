3f19359f9e689455796f18af47bb96741784a478
Fábio Oliveira <fabiodeoliveirabr@gmail.com>
2024-10-15 19:09:19 -0300

    pg cadastrar

--- pagina_cadastrar.php
+++ pagina_cadastrar.php
@@ -1,6 +1,21 @@
-<?php
-    include "verificar_logado.php";
-?>
-
+<?php    include "verificar_logado.php";  ?>
 <!-- pagina_cadastrar.php -->
 <h1>Cadastrar produto</h1>
+<br>
+<form action="" method="post">
+    <label>Nome do produto:</label><br>
+    <input type="text" name="descricao_digitada"> <br><br>
+
+    <label>Preço:</label><br>
+    <input type="number" step="0.01" name="preco_digitado"> <br><br>
+
+    <label>Estoque:</label><br>
+    <input type="number" name="estoque_digitado"> <br><br>
+
+    <label>Foto:</label><br>
+    <input type="text" name="foto_escolhida"> <br><br>
+    
+    
+
+
+</form>
